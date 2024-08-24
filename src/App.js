85b769904f639806e7c5ef27aeeeed6a3a4fd7bc
Karolayne Rodrigues <99221907+Karollayneer@users.noreply.github.com>
2024-08-24 14:41:39 -0300

    bom1
    quase completo uma boa verson

--- src/App.js
+++ src/App.js
@@ -5,18 +5,31 @@ import Project from './components/Project';
 import Footer from './components/Footer';
 import './App.css';
 
+
 function App() {
   return (
-    <div>
+    <div id="root">
       <Header />
-      <maim>
-      <Sobre />
-      <Project title="Projeto 1" description="Descrição do Projeto 1" />
-      <Project title="Projeto 2" description="Descrição do Projeto 2" />
-      </maim>
+      <main>
+        <Sobre />
+        <Project
+          title="Recicla ai"
+          description="Descrição do Projeto 1. Este projeto envolveu o desenvolvimento de uma aplicação web com funcionalidades avançadas."
+          image="/images/projeto1.jpg"  // Caminho para a imagem do projeto 1
+          link="https://github.com/Karollayneer/PGM6-Resilia"  // Link para o projeto 1
+        />
+        <Project
+          title="Projeto 2"
+          description="Descrição do Projeto 2. Um projeto desafiador que focou na análise de dados e criação de relatórios personalizados."
+          image="/images/projeto2.jpg"  // Caminho para a imagem do projeto 2
+          link="https://link-para-o-projeto2.com"  // Link para o projeto 2
+        />
+      </main>
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Project.css'; // Importa o CSS do projeto
 
-function Project({ title, description }) {
+function Project({ title, description, image, link }) {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
+    <div className="project">
+      <a href={link} target="_blank" rel="noopener noreferrer" className="imageContainer">
+        <img src={image} alt={title} className="image" />
+      </a>
+      <h2 className="title">{title}</h2>
+      <p className="description">{description}</p>
     </div>
   );
 }
